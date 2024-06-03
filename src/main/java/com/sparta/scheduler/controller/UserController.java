842a9cd5a01952fb291c89a50d397fec03b782cf
seungsuuu <tmdtn013@naver.com>
2024-06-03 20:32:39 +0900

    피드백3 : UserController 에 있던 JWT 검증 API 를 AuthController 로 분리

--- src/main/java/com/sparta/scheduler/controller/UserController.java
+++ src/main/java/com/sparta/scheduler/controller/UserController.java
@@ -2,26 +2,23 @@ package com.sparta.scheduler.controller;
 
 import com.sparta.scheduler.dto.LoginRequestDto;
 import com.sparta.scheduler.dto.SignupRequestDto;
-import com.sparta.scheduler.entity.UserRole;
-import com.sparta.scheduler.jwt.JwtUtil;
 import com.sparta.scheduler.service.UserService;
-import io.jsonwebtoken.Claims;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    private final JwtUtil jwtUtil;
     private final UserService userService;
 
-    public UserController(JwtUtil jwtUtil, UserService userService) {
-        this.jwtUtil = jwtUtil;
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -39,40 +36,5 @@ public class UserController {
         userService.login(requestDto, res);
 
         return new ResponseEntity<>("로그인이 완료되었습니다.", HttpStatus.valueOf(200));
-    }
-
-    // JWT 생성 및 저장
-    @GetMapping("/create-jwt")
-    public String createJwt(HttpServletResponse res) {
-        // Jwt 생성
-        String token = jwtUtil.createToken("Robbie", UserRole.USER);
-
-        // Jwt 쿠키 저장
-        res.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
-
-        return token;
-    }
-
-    // JWT 가져오기
-    @GetMapping("/get-jwt")
-    public String getJwt(HttpServletRequest request) {
-        // Header 에 있는 JWT 토큰 가져와서 substring
-        String token = jwtUtil.getJwtFromHeader(request);
-
-        // 토큰 검증
-        if (!jwtUtil.validateToken(token)) {
-            throw new IllegalArgumentException("Token Error");
-        }
-
-        // 토큰에서 사용자 정보 가져오기
-        Claims info = jwtUtil.getUserInfoFromToken(token);
-        // 사용자 username
-        String username = info.getSubject();
-        System.out.println("username = " + username);
-        // 사용자 권한
-        String authority = (String) info.get(JwtUtil.AUTHORIZATION_KEY);
-        System.out.println("authority = " + authority);
-
-        return "getJwt : " + username + ", " + authority;
     }
 }
